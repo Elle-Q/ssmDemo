4e3c8c09a76904c4e466484dc02c3a91af432d33
elle <1127198682@qq.com>
2019-09-08 22:26:21 +0800

    ssm demo

---
+++ src/main/test/com/qxy/service/PicServiceTest.java
@@ -0,0 +1,33 @@
+package com.qxy.service;
+
+import com.qxy.db.model.PicDb;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * User: pretty el
+ * Date: 2019/9/8
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring.xml",
+        "classpath:spring-mybatis.xml" })
+public class PicServiceTest {
+
+    @Autowired
+    private PicService picService;
+
+    @Test
+    public void page() {
+//        PicService picService = new PicService();
+        List<PicDb> picDbs = picService.page(1, 1);
+        Assert.assertNotNull(picDbs);
+    }
+}
